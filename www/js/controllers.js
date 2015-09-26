90b29a3a1a1119f42c6bdf5115173fabd21cc93d
ChristianKara <Christian.Kara90@gmail.com>
2015-09-25 20:33:45 -0400

    Populating correct data to each section

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -59,7 +59,35 @@ angular.module('stockWatch.controllers', [])
     ];
 }])
 
-.controller('StockCtrl', ['$scope', '$stateParams',
-   function($scope, $stateParams) {
-  $scope.ticker=$stateParams.stockTicker;
+.controller('StockCtrl', ['$scope', '$stateParams', 'stockDataService',
+   function($scope, $stateParams, stockDataService) {
+     $scope.ticker = $stateParams.stockTicker;
+
+     $scope.$on('$ionicView.afterEnter', function(){
+       getPriceData();
+       getDetailsData();
+     });
+
+     function getPriceData(){
+
+     var promise = stockDataService.getPriceData($scope.ticker);
+     promise.then(function(data){
+
+       console.log(data);
+
+       $scope.stockPriceData = data;
+     });
+   }
+
+   function getDetailsData(){
+
+   var promise = stockDataService.getDetailsData($scope.ticker);
+   promise.then(function(data){
+
+     console.log(data);
+
+     $scope.stockDetailsData = data;
+   });
+   }
+
 }]);
